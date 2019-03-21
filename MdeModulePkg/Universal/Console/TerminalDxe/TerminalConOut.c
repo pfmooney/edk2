zxzharmlesszxz/uefi-edk2

Initial import of bhyve UEFI patch.

MdeModulePkg/Universal/Console/TerminalDxe/TerminalConOut.c
@@ -21,6 +21,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 //
 //
 UNICODE_TO_CHAR  UnicodeToPcAnsiOrAscii[] = {
+#ifdef NOT_BHYVE
   { BOXDRAW_HORIZONTAL,                 0xc4, L'-' },
   { BOXDRAW_VERTICAL,                   0xb3, L'|' },
   { BOXDRAW_DOWN_RIGHT,                 0xda, L'/' },
@@ -74,6 +75,61 @@ UNICODE_TO_CHAR  UnicodeToPcAnsiOrAscii[] = {
   { ARROW_UP,                           0x18, L'^' },
   { ARROW_RIGHT,                        0x3e, L'>' },
   { ARROW_DOWN,                         0x19, L'v' },
+#else
+  { BOXDRAW_HORIZONTAL,                 'q', L'-' },
+  { BOXDRAW_VERTICAL,                   'x', L'|' },
+  { BOXDRAW_DOWN_RIGHT,                 'l', L'/' },
+  { BOXDRAW_DOWN_LEFT,                  'k', L'\\' },
+  { BOXDRAW_UP_RIGHT,                   'm', L'\\' },
+  { BOXDRAW_UP_LEFT,                    'j', L'/' },
+  { BOXDRAW_VERTICAL_RIGHT,             'x', L'|' },
+  { BOXDRAW_VERTICAL_LEFT,              'x', L'|' },
+  { BOXDRAW_DOWN_HORIZONTAL,            'w', L'+' },
+  { BOXDRAW_UP_HORIZONTAL,              'v', L'+' },
+  { BOXDRAW_VERTICAL_HORIZONTAL,        'n', L'+' },
+  { BOXDRAW_DOUBLE_HORIZONTAL,          'q', L'-' },
+  { BOXDRAW_DOUBLE_VERTICAL,            'x', L'|' },
+  { BOXDRAW_DOWN_RIGHT_DOUBLE,          'l', L'/' },
+  { BOXDRAW_DOWN_DOUBLE_RIGHT,          'l', L'/' },
+  { BOXDRAW_DOUBLE_DOWN_RIGHT,          'l', L'/' },
+  { BOXDRAW_DOWN_LEFT_DOUBLE,           'k', L'\\' },
+  { BOXDRAW_DOWN_DOUBLE_LEFT,           'k', L'\\' },
+  { BOXDRAW_DOUBLE_DOWN_LEFT,           'k', L'\\' },
+  { BOXDRAW_UP_RIGHT_DOUBLE,            'm', L'\\' },
+  { BOXDRAW_UP_DOUBLE_RIGHT,            'm', L'\\' },
+  { BOXDRAW_DOUBLE_UP_RIGHT,            'm', L'\\' },
+  { BOXDRAW_UP_LEFT_DOUBLE,             'j', L'/' },
+  { BOXDRAW_UP_DOUBLE_LEFT,             'j', L'/' },
+  { BOXDRAW_DOUBLE_UP_LEFT,             'j', L'/' },
+  { BOXDRAW_VERTICAL_RIGHT_DOUBLE,      't', L'|' },
+  { BOXDRAW_VERTICAL_DOUBLE_RIGHT,      't', L'|' },
+  { BOXDRAW_DOUBLE_VERTICAL_RIGHT,      't', L'|' },
+  { BOXDRAW_VERTICAL_LEFT_DOUBLE,       'u', L'|' },
+  { BOXDRAW_VERTICAL_DOUBLE_LEFT,       'u', L'|' },
+  { BOXDRAW_DOUBLE_VERTICAL_LEFT,       'u', L'|' },
+  { BOXDRAW_DOWN_HORIZONTAL_DOUBLE,     'w', L'+' },
+  { BOXDRAW_DOWN_DOUBLE_HORIZONTAL,     'w', L'+' },
+  { BOXDRAW_DOUBLE_DOWN_HORIZONTAL,     'w', L'+' },
+  { BOXDRAW_UP_HORIZONTAL_DOUBLE,       'v', L'+' },
+  { BOXDRAW_UP_DOUBLE_HORIZONTAL,       'v', L'+' },
+  { BOXDRAW_DOUBLE_UP_HORIZONTAL,       'v', L'+' },
+  { BOXDRAW_VERTICAL_HORIZONTAL_DOUBLE, 'n', L'+' },
+  { BOXDRAW_VERTICAL_DOUBLE_HORIZONTAL, 'n', L'+' },
+  { BOXDRAW_DOUBLE_VERTICAL_HORIZONTAL, 'n', L'+' },
+
+  { BLOCKELEMENT_FULL_BLOCK,            '_', L'*' },
+  { BLOCKELEMENT_LIGHT_SHADE,           'a', L'+' },
+
+  { GEOMETRICSHAPE_UP_TRIANGLE,         0x00, L'^' },
+  { GEOMETRICSHAPE_RIGHT_TRIANGLE,      0x00, L'>' },
+  { GEOMETRICSHAPE_DOWN_TRIANGLE,       0x00, L'v' },
+  { GEOMETRICSHAPE_LEFT_TRIANGLE,       0x00, L'<' },
+
+  { ARROW_LEFT,                         0x00, L'<' },
+  { ARROW_UP,                           0x00, L'^' },
+  { ARROW_RIGHT,                        0x00, L'>' },
+  { ARROW_DOWN,                         0x00, L'v' },
+#endif
 
   { 0x0000,                             0x00, L'\0' }
 };
@@ -143,7 +199,9 @@ TerminalConOutReset (
     }
   }
 
+#ifdef NOT_BHYVE
   This->SetAttribute (This, EFI_TEXT_ATTR (This->Mode->Attribute & 0x0F, EFI_BLACK));
+#endif
 
   Status = This->SetMode (This, 0);
 
@@ -183,6 +241,9 @@ TerminalConOutOutputString (
   UTF8_CHAR                   Utf8Char;
   CHAR8                       GraphicChar;
   CHAR8                       AsciiChar;
+#ifndef NOT_BHYVE
+  CHAR8                       GraphicSeq[8];
+#endif
   EFI_STATUS                  Status;
   UINT8                       ValidBytes;
   //
@@ -246,8 +307,12 @@ TerminalConOutOutputString (
 
         AsciiChar = GraphicChar;
 
+#ifndef NOT_BHYVE
+        GraphicChar = 0;
+#endif
       }
 
+#ifdef NOT_BHYVE
       if (TerminalDevice->TerminalType != PCANSITYPE) {
         GraphicChar = AsciiChar;
       }
@@ -259,6 +324,34 @@ TerminalConOutOutputString (
                                           &Length,
                                           &GraphicChar
                                           );
+#else
+      if (TerminalDevice->TerminalType == PCANSITYPE) {
+        GraphicSeq[0] = AsciiChar;
+        Length = 1;
+      } else {
+        if (GraphicChar == 0) {
+          GraphicSeq[0] = AsciiChar;
+          Length = 1;
+        } else {
+          // For graphical characters, change to DEC G0 set and write
+          // character, followed by reset to ASCII character set
+          GraphicSeq[0] = 0x1b;
+          GraphicSeq[1] = '(';
+          GraphicSeq[2] = '0';
+          GraphicSeq[3] = GraphicChar;
+          GraphicSeq[4] = 0x1b;
+          GraphicSeq[5] = '(';
+          GraphicSeq[6] = 'B';
+          Length = 7;
+        }
+      }
+
+      Status = TerminalDevice->SerialIo->Write (
+                                          TerminalDevice->SerialIo,
+                                          &Length,
+                                          GraphicSeq
+                                          );
+#endif
 
       if (EFI_ERROR (Status)) {
         goto OutputError;
@@ -543,6 +636,7 @@ TerminalConOutSetAttribute (
   //  convert Attribute value to terminal emulator
   //  understandable foreground color
   //
+#ifdef NOT_BHYVE
   switch (Attribute & 0x07) {
 
   case EFI_BLACK:
@@ -580,12 +674,14 @@ TerminalConOutSetAttribute (
     break;
 
   }
+#endif
   //
   //  bit4 of the Attribute indicates bright control
   //  of terminal emulator.
   //
   BrightControl = (UINT8) ((Attribute >> 3) & 1);
 
+#ifdef NOT_BHYVE
   //
   //  convert Attribute value to terminal emulator
   //  understandable background color.
@@ -626,6 +722,19 @@ TerminalConOutSetAttribute (
     BackgroundControl = 47;
     break;
   }
+#endif
+
+#ifndef NOT_BHYVE
+  if (Attribute == 0x0A)
+    ForegroundControl = 31;  /* Highlighted item */
+  else if (BrightControl)
+    ForegroundControl = 32;  /* Bright item */
+  else
+    ForegroundControl = 39;  /* Remove color attr for everything else */
+  BackgroundControl = 49;
+#endif
+
+
   //
   // terminal emulator's control sequence to set attributes
   //

pfmooney/edk2

[d.scott.phillips@intel.com] This is an attempt to reconstruct the
history of the bhyve patch by rephrasing it as a delta against
OvmfPkg and unsquashing various upstream commits that were
squashed into it. In particular, this is commit:
28b621aeb5 ("Initial import of bhyve UEFI patch.")
with the following commits unsquashed from it:
6646b4382a ("OvmfPkg: Update PlatformBaseDebugLibIoPort library")
70e46f44cd ("OvmfPkg/ResetVector: Remove pre-built binaries")
497cbb530a ("OvmfPkg: Build OVMF ResetVector during EDK II build process")
9b9fdbfa70 ("OvmfPkg: Support building OVMF's ResetVector during the EDK II build")
f4a8ab280e ("OvmfPkg/Csm/CsmSupportLib: fix "missing braces around initializer"")
56f65ed838 ("OvmfPkg: Make the VirtIo devices use the new VIRTIO_DEVICE_PROTOCOL")

MdeModulePkg/Universal/Console/TerminalDxe/TerminalConOut.c
@@ -22,59 +22,59 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 //
 //
 UNICODE_TO_CHAR  UnicodeToPcAnsiOrAscii[] = {
-  { BOXDRAW_HORIZONTAL,                 0xc4, L'-' },
-  { BOXDRAW_VERTICAL,                   0xb3, L'|' },
-  { BOXDRAW_DOWN_RIGHT,                 0xda, L'/' },
-  { BOXDRAW_DOWN_LEFT,                  0xbf, L'\\' },
-  { BOXDRAW_UP_RIGHT,                   0xc0, L'\\' },
-  { BOXDRAW_UP_LEFT,                    0xd9, L'/' },
-  { BOXDRAW_VERTICAL_RIGHT,             0xc3, L'|' },
-  { BOXDRAW_VERTICAL_LEFT,              0xb4, L'|' },
-  { BOXDRAW_DOWN_HORIZONTAL,            0xc2, L'+' },
-  { BOXDRAW_UP_HORIZONTAL,              0xc1, L'+' },
-  { BOXDRAW_VERTICAL_HORIZONTAL,        0xc5, L'+' },
-  { BOXDRAW_DOUBLE_HORIZONTAL,          0xcd, L'-' },
-  { BOXDRAW_DOUBLE_VERTICAL,            0xba, L'|' },
-  { BOXDRAW_DOWN_RIGHT_DOUBLE,          0xd5, L'/' },
-  { BOXDRAW_DOWN_DOUBLE_RIGHT,          0xd6, L'/' },
-  { BOXDRAW_DOUBLE_DOWN_RIGHT,          0xc9, L'/' },
-  { BOXDRAW_DOWN_LEFT_DOUBLE,           0xb8, L'\\' },
-  { BOXDRAW_DOWN_DOUBLE_LEFT,           0xb7, L'\\' },
-  { BOXDRAW_DOUBLE_DOWN_LEFT,           0xbb, L'\\' },
-  { BOXDRAW_UP_RIGHT_DOUBLE,            0xd4, L'\\' },
-  { BOXDRAW_UP_DOUBLE_RIGHT,            0xd3, L'\\' },
-  { BOXDRAW_DOUBLE_UP_RIGHT,            0xc8, L'\\' },
-  { BOXDRAW_UP_LEFT_DOUBLE,             0xbe, L'/' },
-  { BOXDRAW_UP_DOUBLE_LEFT,             0xbd, L'/' },
-  { BOXDRAW_DOUBLE_UP_LEFT,             0xbc, L'/' },
-  { BOXDRAW_VERTICAL_RIGHT_DOUBLE,      0xc6, L'|' },
-  { BOXDRAW_VERTICAL_DOUBLE_RIGHT,      0xc7, L'|' },
-  { BOXDRAW_DOUBLE_VERTICAL_RIGHT,      0xcc, L'|' },
-  { BOXDRAW_VERTICAL_LEFT_DOUBLE,       0xb5, L'|' },
-  { BOXDRAW_VERTICAL_DOUBLE_LEFT,       0xb6, L'|' },
-  { BOXDRAW_DOUBLE_VERTICAL_LEFT,       0xb9, L'|' },
-  { BOXDRAW_DOWN_HORIZONTAL_DOUBLE,     0xd1, L'+' },
-  { BOXDRAW_DOWN_DOUBLE_HORIZONTAL,     0xd2, L'+' },
-  { BOXDRAW_DOUBLE_DOWN_HORIZONTAL,     0xcb, L'+' },
-  { BOXDRAW_UP_HORIZONTAL_DOUBLE,       0xcf, L'+' },
-  { BOXDRAW_UP_DOUBLE_HORIZONTAL,       0xd0, L'+' },
-  { BOXDRAW_DOUBLE_UP_HORIZONTAL,       0xca, L'+' },
-  { BOXDRAW_VERTICAL_HORIZONTAL_DOUBLE, 0xd8, L'+' },
-  { BOXDRAW_VERTICAL_DOUBLE_HORIZONTAL, 0xd7, L'+' },
-  { BOXDRAW_DOUBLE_VERTICAL_HORIZONTAL, 0xce, L'+' },
+  { BOXDRAW_HORIZONTAL,                 'q', L'-' },
+  { BOXDRAW_VERTICAL,                   'x', L'|' },
+  { BOXDRAW_DOWN_RIGHT,                 'l', L'/' },
+  { BOXDRAW_DOWN_LEFT,                  'k', L'\\' },
+  { BOXDRAW_UP_RIGHT,                   'm', L'\\' },
+  { BOXDRAW_UP_LEFT,                    'j', L'/' },
+  { BOXDRAW_VERTICAL_RIGHT,             'x', L'|' },
+  { BOXDRAW_VERTICAL_LEFT,              'x', L'|' },
+  { BOXDRAW_DOWN_HORIZONTAL,            'w', L'+' },
+  { BOXDRAW_UP_HORIZONTAL,              'v', L'+' },
+  { BOXDRAW_VERTICAL_HORIZONTAL,        'n', L'+' },
+  { BOXDRAW_DOUBLE_HORIZONTAL,          'q', L'-' },
+  { BOXDRAW_DOUBLE_VERTICAL,            'x', L'|' },
+  { BOXDRAW_DOWN_RIGHT_DOUBLE,          'l', L'/' },
+  { BOXDRAW_DOWN_DOUBLE_RIGHT,          'l', L'/' },
+  { BOXDRAW_DOUBLE_DOWN_RIGHT,          'l', L'/' },
+  { BOXDRAW_DOWN_LEFT_DOUBLE,           'k', L'\\' },
+  { BOXDRAW_DOWN_DOUBLE_LEFT,           'k', L'\\' },
+  { BOXDRAW_DOUBLE_DOWN_LEFT,           'k', L'\\' },
+  { BOXDRAW_UP_RIGHT_DOUBLE,            'm', L'\\' },
+  { BOXDRAW_UP_DOUBLE_RIGHT,            'm', L'\\' },
+  { BOXDRAW_DOUBLE_UP_RIGHT,            'm', L'\\' },
+  { BOXDRAW_UP_LEFT_DOUBLE,             'j', L'/' },
+  { BOXDRAW_UP_DOUBLE_LEFT,             'j', L'/' },
+  { BOXDRAW_DOUBLE_UP_LEFT,             'j', L'/' },
+  { BOXDRAW_VERTICAL_RIGHT_DOUBLE,      't', L'|' },
+  { BOXDRAW_VERTICAL_DOUBLE_RIGHT,      't', L'|' },
+  { BOXDRAW_DOUBLE_VERTICAL_RIGHT,      't', L'|' },
+  { BOXDRAW_VERTICAL_LEFT_DOUBLE,       'u', L'|' },
+  { BOXDRAW_VERTICAL_DOUBLE_LEFT,       'u', L'|' },
+  { BOXDRAW_DOUBLE_VERTICAL_LEFT,       'u', L'|' },
+  { BOXDRAW_DOWN_HORIZONTAL_DOUBLE,     'w', L'+' },
+  { BOXDRAW_DOWN_DOUBLE_HORIZONTAL,     'w', L'+' },
+  { BOXDRAW_DOUBLE_DOWN_HORIZONTAL,     'w', L'+' },
+  { BOXDRAW_UP_HORIZONTAL_DOUBLE,       'v', L'+' },
+  { BOXDRAW_UP_DOUBLE_HORIZONTAL,       'v', L'+' },
+  { BOXDRAW_DOUBLE_UP_HORIZONTAL,       'v', L'+' },
+  { BOXDRAW_VERTICAL_HORIZONTAL_DOUBLE, 'n', L'+' },
+  { BOXDRAW_VERTICAL_DOUBLE_HORIZONTAL, 'n', L'+' },
+  { BOXDRAW_DOUBLE_VERTICAL_HORIZONTAL, 'n', L'+' },
 
-  { BLOCKELEMENT_FULL_BLOCK,            0xdb, L'*' },
-  { BLOCKELEMENT_LIGHT_SHADE,           0xb0, L'+' },
+  { BLOCKELEMENT_FULL_BLOCK,            '_', L'*' },
+  { BLOCKELEMENT_LIGHT_SHADE,           'a', L'+' },
 
-  { GEOMETRICSHAPE_UP_TRIANGLE,         '^', L'^' },
-  { GEOMETRICSHAPE_RIGHT_TRIANGLE,      '>', L'>' },
-  { GEOMETRICSHAPE_DOWN_TRIANGLE,       'v', L'v' },
-  { GEOMETRICSHAPE_LEFT_TRIANGLE,       '<', L'<' },
+  { GEOMETRICSHAPE_UP_TRIANGLE,         0x00, L'^' },
+  { GEOMETRICSHAPE_RIGHT_TRIANGLE,      0x00, L'>' },
+  { GEOMETRICSHAPE_DOWN_TRIANGLE,       0x00, L'v' },
+  { GEOMETRICSHAPE_LEFT_TRIANGLE,       0x00, L'<' },
 
-  { ARROW_LEFT,                         '<', L'<' },
-  { ARROW_UP,                           '^', L'^' },
-  { ARROW_RIGHT,                        '>', L'>' },
-  { ARROW_DOWN,                         'v', L'v' },
+  { ARROW_LEFT,                         0x00, L'<' },
+  { ARROW_UP,                           0x00, L'^' },
+  { ARROW_RIGHT,                        0x00, L'>' },
+  { ARROW_DOWN,                         0x00, L'v' },
 
   { 0x0000,                             0x00, L'\0' }
 };
@@ -146,8 +146,6 @@ TerminalConOutReset (
     }
   }
 
-  This->SetAttribute (This, EFI_TEXT_ATTR (This->Mode->Attribute & 0x0F, EFI_BLACK));
-
   Status = This->SetMode (This, 0);
 
   return Status;
@@ -186,6 +184,7 @@ TerminalConOutOutputString (
   UTF8_CHAR                   Utf8Char;
   CHAR8                       GraphicChar;
   CHAR8                       AsciiChar;
+  CHAR8                       GraphicSeq[8];
   EFI_STATUS                  Status;
   UINT8                       ValidBytes;
   CHAR8                       CrLfStr[2];
@@ -251,18 +250,34 @@ TerminalConOutOutputString (
 
         AsciiChar = GraphicChar;
 
+        GraphicChar = 0;
       }
 
-      if (TerminalDevice->TerminalType != TerminalTypePcAnsi) {
-        GraphicChar = AsciiChar;
+      if (TerminalDevice->TerminalType == TerminalTypePcAnsi) {
+        GraphicSeq[0] = AsciiChar;
+        Length = 1;
+      } else {
+        if (GraphicChar == 0) {
+          GraphicSeq[0] = AsciiChar;
+          Length = 1;
+        } else {
+          // For graphical characters, change to DEC G0 set and write
+          // character, followed by reset to ASCII character set
+          GraphicSeq[0] = 0x1b;
+          GraphicSeq[1] = '(';
+          GraphicSeq[2] = '0';
+          GraphicSeq[3] = GraphicChar;
+          GraphicSeq[4] = 0x1b;
+          GraphicSeq[5] = '(';
+          GraphicSeq[6] = 'B';
+          Length = 7;
+        }
       }
-
-      Length = 1;
 
       Status = TerminalDevice->SerialIo->Write (
                                           TerminalDevice->SerialIo,
                                           &Length,
-                                          &GraphicChar
+                                          GraphicSeq
                                           );
 
       if (EFI_ERROR (Status)) {
@@ -571,92 +586,19 @@ TerminalConOutSetAttribute (
   }
 
   //
-  //  convert Attribute value to terminal emulator
-  //  understandable foreground color
-  //
-  switch (Attribute & 0x07) {
-
-  case EFI_BLACK:
-    ForegroundControl = 30;
-    break;
-
-  case EFI_BLUE:
-    ForegroundControl = 34;
-    break;
-
-  case EFI_GREEN:
-    ForegroundControl = 32;
-    break;
-
-  case EFI_CYAN:
-    ForegroundControl = 36;
-    break;
-
-  case EFI_RED:
-    ForegroundControl = 31;
-    break;
-
-  case EFI_MAGENTA:
-    ForegroundControl = 35;
-    break;
-
-  case EFI_BROWN:
-    ForegroundControl = 33;
-    break;
-
-  default:
-
-  case EFI_LIGHTGRAY:
-    ForegroundControl = 37;
-    break;
-
-  }
-  //
   //  bit4 of the Attribute indicates bright control
   //  of terminal emulator.
   //
   BrightControl = (UINT8) ((Attribute >> 3) & 1);
 
-  //
-  //  convert Attribute value to terminal emulator
-  //  understandable background color.
-  //
-  switch ((Attribute >> 4) & 0x07) {
+  if (Attribute == 0x0A)
+    ForegroundControl = 31;  /* Highlighted item */
+  else if (BrightControl)
+    ForegroundControl = 32;  /* Bright item */
+  else
+    ForegroundControl = 39;  /* Remove color attr for everything else */
+  BackgroundControl = 49;
 
-  case EFI_BLACK:
-    BackgroundControl = 40;
-    break;
-
-  case EFI_BLUE:
-    BackgroundControl = 44;
-    break;
-
-  case EFI_GREEN:
-    BackgroundControl = 42;
-    break;
-
-  case EFI_CYAN:
-    BackgroundControl = 46;
-    break;
-
-  case EFI_RED:
-    BackgroundControl = 41;
-    break;
-
-  case EFI_MAGENTA:
-    BackgroundControl = 45;
-    break;
-
-  case EFI_BROWN:
-    BackgroundControl = 43;
-    break;
-
-  default:
-
-  case EFI_LIGHTGRAY:
-    BackgroundControl = 47;
-    break;
-  }
   //
   // terminal emulator's control sequence to set attributes
   //